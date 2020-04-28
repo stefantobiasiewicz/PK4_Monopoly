5bb403df59489c693decb5dcc2e758bd01a44413
alefron <ola.front@gmail.com>
2020-04-28 22:43:08 +0200

    com1

---
+++ Monopoly/Monopoly/karta.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+
+
+
+int main()
+{
+
+
+	std::cout << "dziala\n\r";
+
+
+	return 0;
+}
--- Monopoly/Monopoly/main.cpp
+++ Monopoly/Monopoly/main.cpp
@@ -9,6 +9,5 @@ int main()
 
 	std::cout << "jeœli to widzisz to napisz do mnie czy to dzia³a ?\n\r";
 
-
 	return 0;
 }
